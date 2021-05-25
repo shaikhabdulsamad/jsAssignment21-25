476a54b2b193af41a5a6f419470dbe72cab45387
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-05-25 07:01:56 +0500

    js(21-25)commit1

---
+++ app.js
@@ -0,0 +1,125 @@
+
+//********************** Chapter : 21-25 ***********************/
+
+
+//********************** Task : 01 ***********************/
+
+// var firstName = prompt('Enter your first name');
+// var lastName = prompt('Enter your last name');
+// var fullName = firstName  + lastName
+// alert(fullName);
+
+
+//********************** Task : 02 ***********************/
+
+// var userInput = prompt('Enter your favorite Mobile Phone Model');
+// var userInputLength = userInput.length;
+// alert('My favorite Phone is: '+ userInput +'\n'+'Length of string: '+ userInputLength)
+
+
+//********************** Task : 03 ***********************/
+
+// var str = 'Pakistani';
+// var index = str.indexOf('n');
+// alert('String : '+str +'\n'+'Index of "n" is : '+index)
+
+
+//********************** Task : 04 ***********************/
+
+// var str = 'Hello World';
+// var index = str.lastIndexOf('l');
+// alert('String : '+str +'\n'+'Last index of "l" is : '+index)
+
+
+//********************** Task : 05 ***********************/
+
+// var str = 'Pakistani';
+// var index = str.charAt(3);
+// alert('String : '+str +'\n'+'Character at 3rd index is : '+index)
+
+
+//********************** Task : 06 ***********************/
+
+// var firstName = prompt('Enter your first name');
+// var lastName = prompt('Enter your last name');
+// var fullName = firstName.concat(" "+ lastName) 
+// alert(fullName);
+
+
+//********************** Task : 07 ***********************/
+
+// var city = 'Hyderabad';
+// var change = city.replace('Hyderabad' ,'Islamabad')
+// alert('City : '+ city+'\n'+ 'After replacement : '+change)
+
+
+//********************** Task : 08 ***********************/
+
+// var message = "Ali and Sami are best friends. They play cricket and football together."
+
+// message = message.replace(/and/g,"&")
+// alert(message)
+
+
+//********************** Task : 09 ***********************/
+
+// var str = "472";
+// var num = Number(str);
+// alert("string: " + str +"\n"+ "Number : " + num)
+
+
+//********************** Task : 10 ***********************/
+
+// var userInput = prompt("Enter your word");
+// var changeCase = userInput.toUpperCase();
+// alert("User Input : " + userInput +"\n" + "UpperCase : " + changeCase)
+
+
+
+//********************** Task : 11 ***********************/
+
+// var userInput = prompt('enter your name');
+
+// console.log(userInput.slice(0,1).toUpperCase() +userInput.slice(1));
+
+
+
+//********************** Task : 14 ***********************/
+
+// var arr = ["cake", "apple pie", "cookie", "chips", "patties"]
+
+// var userInput = prompt('enter your name').toLowerCase();
+
+// flag = false
+
+// for(var i = 0; i < arr.length; i++){
+
+//     if(userInput === arr[i]){
+//         flag = true
+//         alert(userInput + " is available at index " + i + " in our bakery")
+// break
+//     }
+// }
+// if(flag === false){
+//     alert("We are sorry " + userInput + " is not available in our bakery")
+// }
+
+
+
+//********************** Task : 16 ***********************/
+
+// var str = "University of Karachi";
+
+// for(var i = 0; i < str.length; i++){
+//     var arr = str[i].split().join()
+    
+//     console.log(arr)
+// }
+
+
+
+//********************** Task : 17 ***********************/
+
+// var userInput = prompt("Enter Your word");
+// var lastChar = userInput.charAt(userInput.length-1)
+// alert("User input : "+ userInput +"\n"+ "Last Character of input : "+ lastChar )
